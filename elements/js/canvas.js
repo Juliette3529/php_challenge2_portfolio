dd31d54a1c43b31db51c9af1e7208805ad1b2a2d
Juliette3529 <Juliette@saint-jalme.fr>
2016-09-15 10:01:10 +0200

    canvas almost done

--- elements/js/canvas.js
+++ elements/js/canvas.js
@@ -2,15 +2,24 @@ window.onload = function()
 {
 var c = document.getElementById("myCanvas");
 var ctx = c.getContext("2d");
+
+//mon texte en canvas
+
+
+//mon octogone
 ctx.beginPath();
 ctx.moveTo(125, 275);//On se déplace au coin inférieur gauche
 ctx.lineTo(35,200 );
 ctx.lineTo(35, 100);
 ctx.lineTo(125, 25);
+ctx.lineTo(200, 25);
+ctx.lineTo(275, 120);
+ctx.lineTo(175, 175);
+ctx.lineTo(125, 275);
 ctx.stroke();//On trace seulement les lignes.
-
 ctx.closePath();
-    ctx.beginPath();
+//debut de mon cercle
+ctx.beginPath();
 ctx.arc(150,150,150,0,2*Math.PI);
 ctx.stroke();
 ctx.closePath();
